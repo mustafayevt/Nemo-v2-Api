a12ff3e72c96f79df40fd1fca5bf4920d3bdb790
Tural Mustafayev <mustafayevtural46@gmail.com>
2020-04-13 22:16:39 +0400

    IngredientsExportDto added

---
+++ Nemo v2 Data/DTO's/IngredientsExportDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Nemo_v2_Data.Entities;
+
+namespace Nemo_v2_Data
+{
+    public class IngredientsExportDto
+    {
+        public long Id { get; set; }
+        [Required]
+        public long IngredientId { get; set; }
+        
+        [Required]
+        public long WarehouseInvoiceId { get; set; }
+        [Required]
+        public decimal Quantity { get; set; }
+        [Required]
+        public decimal PriceForEach { get; set; }
+        
+        [Required,ForeignKey(nameof(Restaurant))]
+        public long RestaurantId { get; set; }
+    }
+}
