fdd2dea1d4a2301dcb3597c819b820f9314a9caf
mustafayevt <mustafayevtural46@gmail.com>
2020-06-03 22:45:33 +0400

    Unit Test project added

---
+++ ApiTest/ControllerTests/BuyerControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Nemo_v2_Api;
+using Xunit;
+
+namespace ApiTest.ControllerTests
+{
+    public class BuyerControllerTest
+    {
+        private readonly HttpClient _httpClient;
+
+        public BuyerControllerTest()
+        {
+            var webHostBuilder =
+                new WebHostBuilder()
+                    .UseEnvironment("Development") 
+                    .UseStartup<Startup>();
+
+            var server = new TestServer(webHostBuilder);
+            _httpClient = server.CreateClient();
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        public async Task GetBuyerById(long id)
+        {
+            
+            //Arrange
+            var request  = new HttpRequestMessage(HttpMethod.Get,$"/api/Buyer/GetBuyer/{id}");
+            
+            //Act
+            var response = await _httpClient.SendAsync(request);
+            
+            //Assert
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.BadRequest,response.StatusCode);
+        }
+    }
+}
---
+++ ApiTest/ServiceTests/BuyerServiceTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Nemo_v2_Data.Entities;
+using Nemo_v2_Repo.Abstraction;
+using Nemo_v2_Service.Services;
+using NSubstitute;
+using Xunit;
+
+namespace ApiTest.ServiceTests
+{
+    public class BuyerServiceTest
+    {
+        private readonly BuyerService _buyerService;
+        private readonly IUnitOfWork _unitOfWork = Substitute.For<IUnitOfWork>();
+
+        public BuyerServiceTest()
+        {
+            _buyerService = new BuyerService(_unitOfWork);
+        }
+
+        [Fact]
+        public async Task GetBuyers_ShouldReturnBuyers_WhenBuyersExists()
+        {
+            //Arrange
+            var buyers = new List<Buyer>
+            {
+                new Buyer
+                {
+                    Id = 1,
+                    Name = "TestBuyer1"
+                },
+                new Buyer
+                {
+                    Id = 2,
+                    Name = "TestBuyer2"
+                }
+            };
+            _unitOfWork.BuyerRepository.Get().Returns(buyers);
+
+            //Act
+            var result = _buyerService.GetBuyers();
+
+            //Assert
+            Assert.Equal(buyers,result);
+        }
+    }
+}
